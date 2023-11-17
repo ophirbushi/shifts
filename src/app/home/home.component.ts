f182e7d59df1cee38afc2d01bde53f24da8e5e44
Ophir Bushinsky <ophirbushi@gmail.com>
2023-11-17 18:49:06 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component } from '@angular/core'
 import { AssignmentExtended, StateService } from '../shared/state.service'
 import { ColDef, GridOptions } from 'ag-grid-community'
@@ -16,15 +17,30 @@ export class HomeComponent {
         defaultColDef: {
             sortable: true,
             filter: 'agTextColumnFilter',
-            menuTabs: ['filterMenuTab']
-        }
+            menuTabs: ['filterMenuTab'],
+        },
+        rowSelection: 'multiple'
     }
-    columnDefs: ColDef<AssignmentExtended>[] = [
-        { field: 'volunteerName', headerName: 'מתנדב' },
-        { field: 'institutionName', headerName: 'מוסד' },
+    columnDefs: ColDef<any>[] = [
+        { field: 'firstName', headerName: 'שם פרטי', checkboxSelection: true, headerCheckboxSelection: true },
+        { field: 'lastName', headerName: 'שם משפחה' },
         { field: 'phone', headerName: 'טלפון' },
-        { field: 'date', headerName: 'תאריך' },
-        { field: 'timeframe', headerName: 'טווח שעות' },
+        { field: 'email', headerName: 'דוא"ל' },
+        { field: 'quantity', headerName: 'כמות' },
+        { field: 'status', headerName: 'תהליך הסתיים' },
+        { headerName: 'עוד מידע', cellRenderer: () => `<button class="btn btn-secondary">?</button>` }
+    ]
+
+    rowData = [
+        { firstName: 'יונתן', lastName: 'איתמר', phone: '054-1112234', email: 'yi@gmail.com', quantity: 4, status: true },
+        { firstName: 'חגית', lastName: 'מיכאל', phone: '054-1112234', email: 'hm@gmail.com', quantity: 12, status: false },
+        { firstName: 'צחי', lastName: 'שי', phone: '054-1112234', email: 'ts@gmail.com', quantity: 9, status: true },
+        { firstName: 'יונתן', lastName: 'איתמר', phone: '054-1112234', email: 'yi@gmail.com', quantity: 4, status: false },
+        { firstName: 'חגית', lastName: 'מיכאל', phone: '054-1112234', email: 'hm@gmail.com', quantity: 12, status: true },
+        { firstName: 'צחי', lastName: 'שי', phone: '054-1112234', email: 'ts@gmail.com', quantity: 9, status: false },
+        { firstName: 'יונתן', lastName: 'איתמר', phone: '054-1112234', email: 'yi@gmail.com', quantity: 4, status: false },
+        { firstName: 'חגית', lastName: 'מיכאל', phone: '054-1112234', email: 'hm@gmail.com', quantity: 12, status: false },
+        { firstName: 'צחי', lastName: 'שי', phone: '054-1112234', email: 'ts@gmail.com', quantity: 9, status: true },
     ]
 
     constructor(private state: StateService) { }
